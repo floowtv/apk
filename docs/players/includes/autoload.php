6b1ea04d08c820d283e8ae691f3a8da7ad297ce0
floowtv <74938529+floowtv@users.noreply.github.com>
2022-10-19 05:20:26 -0300

    Delete docs directory

--- docs/players/includes/autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-if(empty($_SESSION)) session_start();
-require_once('includes/config.php');
-require_once('includes/functions.php');
-?>
--- docs/players/includes/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$db_username = 'crud';
-	$db_password = 'crud';
-	$conn = new PDO( 'mysql:host=localhost;dbname=crud', $db_username, $db_password );
-	if(!$conn){
-		die("Fatal Error: Connection Failed!");
-	}
-?>
